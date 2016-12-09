55a3e3dd2641ae14f34c5c6a0240d7f576f90036
Felix <wintelx264@163.com>
2016-12-09 23:40:11 +0800

    add build script

---
+++ build/config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+current_dir=`pwd`
+build_dir=$current_dir/build_dir
+if ! [ -e  $build_dir ];then
+    mkdir -p $build_dir
+fi
+source_dir=`readlink -f "$current_dir/../source"`
+
+cd $build_dir
+$source_dir/configure \
+    --prefix=$build_dir \
+    --disable-shared \
+    --enable-static \
+
+make && make install
+
+cd -
